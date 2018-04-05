2cf02ffa0cd6b4ef396372812631fcb782c0966c
Napoleon Wulkan <napoleon@wulkan.xyz>
2018-04-05 15:34:08 +0200

    added models for pgsql

--- cmd/gfl/types.go
+++ cmd/gfl/types.go
@@ -17,8 +17,6 @@ var typesCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		client := gontentful.NewClient(&gontentful.ClientOptions{
 			CdnURL: "cdn.contentful.com",
-			//SpaceID:  "dbq0oal15rwl",
-			//CdnToken: "760b2a53f6b785f745e72f087b266e0c3feeb2f203e77dbcc69b8eeaa2922c14",
 			SpaceID:  SpaceId,
 			CdnToken: CdnToken,
 		})
@@ -46,4 +44,134 @@ var typesCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(typesCmd)
+}
+
+type PGSQLSchema struct {
+	SchemaName string
+	Tables []PGSQLTable
+}
+
+func NewPGSQLSchema(schemaName string, items []ContentType) PGSQLSchema{
+	schema := PGSQLSchema{
+		SchemaName: schemaName,
+	}
+
+	tables := make([]PGSQLTable, 0)
+	for _, item := range items {
+		table := NewPGSQLTable(schemaName, item.Sys.ID, item.Fields)
+		tables = append(tables, table)
+	}
+
+	schema.Tables = tables
+
+	return schema
+}
+
+func NewPGSQLTable(schemaName, tableName string, fields []*Field) PGSQLTable {
+	table := PGSQLTable{
+		Referencing: make([]string, 0),
+		ReferencedBy: make([]string, 0),
+		SchemaName: schemaName,
+		TableName: tableName,
+	}
+
+	columns := make([]PGSQLColumn, 0)
+	for _, field := range fields {
+		column := NewPGSQLColumn(table, *field)
+		columns = append(columns, column)
+	}
+
+	table.Columns = columns
+
+	table.Columns[len(table.Columns) - 1].Last = true
+
+	return table
+}
+
+func NewPGSQLColumn(table PGSQLTable, field Field) PGSQLColumn {
+	column := PGSQLColumn{
+		Table: table,
+		SchemaName: table.SchemaName,
+		ColumnName: field.ID,
+	}
+
+	column.ColumnDesc = column.getColumnDesc(field)
+
+	return column
+}
+
+func (c *PGSQLColumn) getColumnDesc(field Field) string {
+	columnType := c.getColumnType(field)
+	//if c.isUnique(field.Validations) {
+	//	columnType += " unique"
+	//}
+	return columnType
+}
+
+func (c *PGSQLColumn) getColumnType(field Field) string {
+	switch field.Type {
+	case "Symbol":
+		return "text"
+      case "Text":
+		return "text"
+      case "Integer":
+		return "integer"
+      case "Number":
+		return "decimal"
+      case "Date":
+		return "date"
+      case "Location":
+		return "point"
+      case "Boolean":
+		return "boolean"
+      case "Link":
+		return c.getLinkType(field.Validations)
+	case "Array":
+		return c.getArrayType(*field.Items)
+	case "Object":
+		return "jsonb"
+	}
+	return ""
+}
+
+func (c *PGSQLColumn) getArrayType(items FieldTypeArrayItem) string {
+	switch items.Type {
+	case "Symbol":
+		return "text ARRAY"
+  	case "Link":
+		return c.getLinkType(items.Validations)
+	}
+	return ""
+}
+
+func (c *PGSQLColumn) getLinkType(validations []FieldValidation) string {
+	if len(validations) > 0 {
+		if len(validations[0].LinkContentType) != 0 {
+			refTable := validations[0].LinkContentType[0]
+			c.Table.Referencing = append(c.Table.Referencing, refTable)
+			return "integer not null references " + c.SchemaName + "." + refTable + "(id)"
+		} else if len(validations[0].LinkMimetypeGroup) != 0 {
+			return "text" // image
+		}
+	} else {
+		return "text"
+    }
+    return ""
+}
+
+
+type PGSQLTable struct {
+	Referencing []string
+	ReferencedBy []string
+	SchemaName string
+	TableName string
+	Columns []PGSQLColumn
+}
+
+type PGSQLColumn struct {
+	Table PGSQLTable
+	SchemaName string
+	ColumnName string
+	ColumnDesc string
+	Last bool
 }
