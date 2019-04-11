86ccc52decad15299db111f4224a7a86086eafe3
speier <kalman.speier@gmail.com>
2019-04-11 12:13:15 +0200

    run script to test schema creation and sync in one go

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git checkout 9401dbeccb7ddaad46caa09f1b9c4131130adc58
+
+cid=$(docker ps -aqf 'name=postgres')
+echo 'DROP SCHEMA IF EXISTS content CASCADE' | docker exec -i $cid psql -U postgres
+
+cd cmd/gfl
+
+echo ''
+go run . schema pg -s $CF_SPACE -t $CF_TOKEN --url postgres://postgres@localhost:5432/?sslmode=disable
+
+echo ''
+go run . sync pg -s $CF_SPACE -t $CF_TOKEN --url postgres://postgres@localhost:5432/?sslmode=disable
+
+echo ''
+echo 'SELECT COUNT(*) FROM content.page_en' | docker exec -i $cid psql -U postgres
