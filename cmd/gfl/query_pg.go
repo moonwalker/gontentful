3b54643a546e15b443dcc6bccedaf092b6f949a7
rucsi <rucsi6@gmail.com>
2019-05-16 14:43:29 -0400

    query filters

--- cmd/gfl/query_pg.go
+++ cmd/gfl/query_pg.go
@@ -13,55 +13,20 @@ import (
 	"github.com/moonwalker/gontentful"
 )
 
+const (
+	DEFAULT_INCLUDE = 3
+	MAX_INCLUDE     = 10
+)
+
 var (
 	runQuery bool
 	test     = []string{
+		"content_type=market&fields.code=ROW&include=2&limit=1&locale=en&skip=0",
+		"content_type=product&fields.name=dreamz&include=3&limit=1&skip=0",
 		"content_type=game&include=3&limit=200&locale=sv&order=-fields.priority&skip=1800",
 		"content_type=game&include=3&limit=200&locale=en-GB&select=sys%2Csys.id%2Cfields.slug&skip=600",
 		"content_type=game&fields.content.fields.name%5Bmatch%5D=cap&fields.content.sys.contentType.sys.id=gameInfo&include=2&limit=200&locale=fi&order=-fields.priority&select=sys%2Cfields.slug%2Cfields.content%2Cfields.deviceConfigurations&skip=0",
 		"content_type=game&fields.slug=winter-wonders&include=3&limit=1&locale=en&skip=0",
-		"content_type=product&query=name%3Ddreamz",
-		"content_type=webComponent",
-		"content_type=phrase&limit=1000&locale=en",
-		"content_type=market&include=2&locale=en&query=code%3DROW",
-		"content_type=page&include=5&locale=en&query=sys.id%3D5mbbjk3zNe8a6MsQS4k6Q0",
-		"content_type=market",
-		"content_type=locale&query=fields.disable%255Bne%255D%3Dtrue",
-		"content_type=routeAlias&locale=en&query=fields.alias%3D%252F",
-		"content_type=product&include=2&query=fields.name%3Ddreamz",
-		"content_type=route&locale=en&query=fields.path%3D%252F",
-		"content_type=locale&query=fields.code%3Den",
-		"content_type=routeAlias&locale=en",
-		"content_type=menu&include=2&locale=en&query=slug%3Dcasino",
-		"content_type=gameStudio&limit=12&locale=en&order=-priority",
-		"content_type=product&include=2&query=fields.name%3Ddreamz",
-		"content_type=gameWildFeature&limit=12&locale=en&order=-priority",
-		"content_type=locale&query=fields.disable%255Bne%255D%3Dtrue",
-		"content_type=menu&include=2&locale=en&query=slug%3Dbottom-navigation",
-		"content_type=route&locale=en&query=fields.path%3D%252F",
-		"content_type=route&locale=en&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=sv&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=en-SE&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=en-GB&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=no&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=menu&include=2&locale=en&query=slug%3Dfooter-safety",
-		"content_type=route&locale=fi&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=de&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=en-CA&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=route&locale=en-NZ&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=menu&include=2&locale=en&query=slug%3Dfooter-payment-providers",
-		"content_type=route&locale=en-EU&query=id%3D426gHfnhHWWKCA2k06GIYc",
-		"content_type=menu&include=2&locale=en&query=slug%3Dfooter-menu",
-		"content_type=tldr&locale=en&query=key%3Dfooter-about",
-		"content_type=tldr&locale=en&query=key%3Dfooter-legal",
-		"content_type=menu&include=2&locale=en&query=slug%3Dfooter-social-links",
-		"content_type=menu&include=2&locale=en&query=slug%3Dtop-navigation-site-desktop",
-		"content_type=menu&include=2&locale=en&query=slug%3Duser-menu-mobile",
-		"content_type=menu&include=2&locale=en&query=slug%3Dside-navigation-site-v2",
-		"content_type=menu&include=2&locale=en&query=slug%3Dside-navigation-cashier",
-		"content_type=menu&include=2&locale=en&query=slug%3Dside-navigation-account",
-		"content_type=menu&include=2&locale=en&query=slug%3Dtop-navigation-user-desktop",
-		"content_type=menu&include=2&locale=en&query=slug%3Dside-navigation-user",
 	}
 )
 
@@ -94,6 +59,12 @@ var pgQueryCmd = &cobra.Command{
 		if len(includeQ) > 0 {
 			include, _ = strconv.Atoi(includeQ)
 		}
+		if include == 0 {
+			include = DEFAULT_INCLUDE
+		}
+		if include > MAX_INCLUDE {
+			include = MAX_INCLUDE
+		}
 
 		skip := 0
 		skipQ := q.Get("skip")
@@ -120,9 +91,8 @@ var pgQueryCmd = &cobra.Command{
 		q.Del("order")
 
 		log.Println("data gathered successfully")
-
-		log.Println("executing query schema...")
 		query := gontentful.NewPGQuery(schemaName, contentType, locale, defaultLocale, fields, q, order, skip, limit, include)
+		log.Println("executing query schema...")
 		err = query.Exec(queryDatabaseURL)
 		if err != nil {
 			log.Fatal(err)
--- query_pg.go
+++ query_pg.go
@@ -4,19 +4,29 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
-	"log"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
-const MetaQueryFormat = `
+const includeQueryFormat = `
+SELECT table_name FROM %s._entries WHERE sys_id = '%s'
+`
+
+const metaQueryFormat = `
 SELECT
 	name,
---	type,
---	link_type,
---	items,
+	type,
+	link_type,
+	coalesce(
+		case
+	  		when items IS NULL then null
+	  		else items ->> 'linkType'
+		end,
+	'') as items,
 	is_localized
 --	is_required,
 --  is_disabled,
@@ -33,17 +43,25 @@ SELECT
 {{- end -}}
 {{- end }}
 FROM {{ $.SchemaName }}.{{ $.TableName }}_{{ $.DefaultLocale }} {{ $.TableName }}_{{ $.DefaultLocale }}
-{{ if ne $.Locale $.DefaultLocale }}LEFT JOIN {{ $.SchemaName }}.{{ $.TableName }}_{{ $.Locale }} {{ $.TableName }}_{{ $.Locale }} ON {{ $.TableName }}_{{ $.DefaultLocale }}.sysid = {{ $.TableName }}_{{ $.Locale }}.sysid{{ end }}
+{{ if ne $.Locale $.DefaultLocale }}LEFT JOIN {{ $.SchemaName }}.{{ $.TableName }}_{{ $.Locale }} {{ $.TableName }}_{{ $.Locale }} ON {{ $.TableName }}_{{ $.DefaultLocale }}.sys_id = {{ $.TableName }}_{{ $.Locale }}.sys_id{{ end }}
 {{ if $.Filters }}WHERE
-{{- range $fkey, $fvalue := $.Filters }}
-
+{{- range $fidx, $filter := $.Filters }}
+{{- if $fidx }} AND {{- end }}
+{{ .Field }} {{ .Comparer }} '{{ .Value }}'
+{{ end -}}
 {{- end -}}
-{{- end -}}
-{{ if $.Order }}ORDER BY {{ $.Order }}{{ end -}}
+{{- if $.Order }}ORDER BY {{ $.Order }}{{ end -}}
+{{- if $.Limit }}LIMIT {{ $.Limit }}{{ end -}}
+{{- if $.Skip }}OFFSET {{ $.Skip }}{{ end -}}
 `
 
 var (
 	comparerRegex = regexp.MustCompile(`.+\[(^\])+]=.+`)
+)
+
+const (
+	LINK  = "Link"
+	ARRAY = "Array"
 )
 
 type PQQueryFilter struct {
@@ -58,8 +76,8 @@ type PGQuery struct {
 	Locale        string
 	DefaultLocale string
 	Fields        []string
-	Filters       []*PQQueryFilter
 	Order         string
+	Filters       []*PQQueryFilter
 	Columns       []*PGSQLMeta
 	Limit         int
 	Skip          int
@@ -85,55 +103,172 @@ func NewPGQuery(schemaName string, tableName string, locale string, defaultLocal
 func (s *PGQuery) Exec(databaseURL string) error {
 	db, _ := sql.Open("postgres", databaseURL)
 
-	metas, err := db.Query(fmt.Sprintf(MetaQueryFormat, s.SchemaName, s.TableName))
+	res, err := s.execute(db, 0)
+	fmt.Println("Exec", res)
+	return err
+}
+
+func (s *PGQuery) execute(db *sql.DB, includeLevel int) (map[string]interface{}, error) {
+	// fmt.Println("executing meta query for", s.TableName)
+	// fmt.Println(fmt.Sprintf(metaQueryFormat, s.SchemaName, s.TableName))
+	metas, err := db.Query(fmt.Sprintf(metaQueryFormat, s.SchemaName, s.TableName))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer metas.Close()
 	fields := make(map[string]struct{})
-	for _, f := range s.Fields {
-		fields[f] = struct{}{}
+	allFields := true
+	if s.Fields != nil && len(s.Fields) > 0 {
+		allFields = false
+		for _, f := range s.Fields {
+			fields[f] = struct{}{}
+		}
 	}
-	allFields := (s.Fields == nil)
 	s.Columns = make([]*PGSQLMeta, 0)
+	includedAssets := make([]int, 0)
+	includedEntries := make([]int, 0)
+	index := 0
 	for metas.Next() {
 		meta := PGSQLMeta{}
-		err := metas.Scan(&meta.Name, &meta.Localized)
+		err := metas.Scan(&meta.Name, &meta.Type, &meta.LinkType, &meta.Items, &meta.Localized)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		_, ok := fields[meta.Name]
 		if allFields || ok {
 			s.Columns = append(s.Columns, &meta)
+			if includeLevel < s.Include {
+				switch getMetaLinkType(meta) {
+				case ENTRY:
+					includedEntries = append(includedEntries, index)
+					break
+				case ASSET:
+					includedAssets = append(includedAssets, index)
+					break
+				}
+			}
 		}
+		index = index + 1
 	}
-
 	tmpl, err := template.New("").Parse(queryTemplate)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var buff bytes.Buffer
 	err = tmpl.Execute(&buff, s)
 	if err != nil {
-		return err
-	}
-
-	txn, err := db.Begin()
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	fmt.Println(buff.String())
-	_, err = txn.Exec(buff.String())
+	res, err := db.Query(buff.String())
 	if err != nil {
-		return err
+		return nil, err
 	}
+	defer res.Close()
+	entry := make(map[string]interface{})
+	for res.Next() {
+		values := make([]interface{}, len(s.Columns))
+		for i := range values {
+			values[i] = new(sql.RawBytes)
+		}
+		err := res.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+		for i, c := range s.Columns {
+			entry[c.Name] = convertToType(values[i].(*sql.RawBytes), c)
+		}
+		if includeLevel < s.Include {
+			err = s.includeAll(db, entry, includedEntries, includedAssets, includeLevel)
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return entry, nil
+}
 
-	return txn.Commit()
+func getMetaLinkType(meta PGSQLMeta) string {
+	switch meta.Type {
+	case LINK:
+		return meta.LinkType
+	case ARRAY:
+		return meta.Items
+	}
+	return ""
+}
+
+func convertToType(bytes *sql.RawBytes, column *PGSQLMeta) interface{} {
+	str := string(*bytes)
+	if str == "" {
+		return nil
+	}
+	switch column.Type {
+	case "Integer":
+		i, _ := strconv.ParseInt(string(*bytes), 10, 64)
+		return i
+	case "Number":
+		f, _ := strconv.ParseFloat(string(*bytes), 64)
+		return f
+	case "Date":
+		d, _ := time.Parse(time.RFC3339, string(*bytes))
+		return d
+	case "Boolean":
+		b, _ := strconv.ParseBool(string(*bytes))
+		return b
+	case "Array":
+		str = strings.TrimPrefix(str, "{")
+		str = strings.TrimSuffix(str, "}")
+		vals := strings.Split(str, ",")
+		switch column.Items {
+		case "Integer":
+			res := make([]int64, 0)
+			for _, v := range vals {
+				i, err := strconv.ParseInt(v, 10, 64)
+				if err == nil {
+					res = append(res, i)
+				}
+			}
+			return res
+		case "Number":
+			res := make([]float64, 0)
+			for _, v := range vals {
+				f, err := strconv.ParseFloat(v, 64)
+				if err == nil {
+					res = append(res, f)
+				}
+			}
+			return res
+		case "Date":
+			res := make([]time.Time, 0)
+			for _, v := range vals {
+				d, err := time.Parse(time.RFC3339, v)
+				if err == nil {
+					res = append(res, d)
+				}
+			}
+			return res
+		case "Boolean":
+			res := make([]bool, 0)
+			for _, v := range vals {
+				b, err := strconv.ParseBool(v)
+				if err == nil {
+					res = append(res, b)
+				}
+			}
+			return res
+		}
+		return vals
+	}
+	return str
 }
 
 func formatOrder(order string) string {
+	if order == "" {
+		return order
+	}
 	value := order
 	desc := ""
 	if order[:1] == "-" {
@@ -146,9 +281,9 @@ func formatOrder(order string) string {
 func formatFilter(filters url.Values) []*PQQueryFilter {
 	clauses := make([]*PQQueryFilter, 0)
 	for key, values := range filters {
-		fmt.Println(key, values)
+		fmt.Println("formatFilter", key, values)
 		f := key
-		c := "+"
+		c := "="
 		comparerMatch := comparerRegex.FindStringSubmatch(f)
 		if len(comparerMatch) > 0 {
 			c = comparerMatch[1]
@@ -166,4 +301,67 @@ func formatFilter(filters url.Values) []*PQQueryFilter {
 		})
 	}
 	return clauses
+}
+
+func (s *PGQuery) includeAll(db *sql.DB, fields map[string]interface{}, includedEntries []int, includedAssets []int, includeLevel int) error {
+	// for _, a := range includedAssets {
+	// 	if fields[a] != nil {
+	// 		txn
+	// 	}
+	// }
+	// fmt.Println("includedEntries", fields)
+	for _, idx := range includedEntries {
+		col := s.Columns[idx]
+		// fmt.Println(idx, s.Columns[idx])
+		if fields[col.Name] != nil {
+			switch col.Type {
+			case ARRAY:
+				f, err := s.getBySysIDs(db, fields[col.Name].([]string), includeLevel)
+				if err != nil {
+					return err
+				}
+				fields[col.Name] = f
+				//fmt.Println("getBySysIDs", col.Name, f)
+				break
+			case LINK:
+				str := fields[col.Name].(string)
+				if str != "" {
+					f, err := s.getBySysIDs(db, []string{str}, includeLevel)
+					if err != nil {
+						return err
+					}
+					fields[col.Name] = f[0]
+					//fmt.Println("getBySysIDs", col.Name, f)
+				}
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (s *PGQuery) getBySysIDs(db *sql.DB, sys_ids []string, includeLevel int) ([]map[string]interface{}, error) {
+	res := make([]map[string]interface{}, 0)
+	for _, id := range sys_ids {
+		filter := url.Values{}
+		filter.Set("sys.id", id)
+		tableName := ""
+		// fmt.Println(fmt.Sprintf(includeQueryFormat, s.SchemaName, id))
+		erow := db.QueryRow(fmt.Sprintf(includeQueryFormat, s.SchemaName, id))
+		err := erow.Scan(&tableName)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				fmt.Println("No rows were returned!", id)
+				continue
+			}
+			return nil, err
+		}
+		q := NewPGQuery(s.SchemaName, tableName, s.Locale, s.DefaultLocale, nil, filter, "", 0, 1, s.Include)
+		r, err := q.execute(db, includeLevel+1)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, r)
+	}
+	return res, nil
 }
