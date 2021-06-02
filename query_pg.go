6bd87fccfb5a523347c40b469e3eaf6c3c4b6181
Roland Helli <helli.roland@gmail.com>
2021-06-02 14:51:53 +0200

    SetMaxIdleConns 50

--- query_pg.go
+++ query_pg.go
@@ -238,7 +238,7 @@ func (s *PGQuery) Exec(databaseURL string) (int64, string, error) {
 	once.Do(func() {
 		db, err = sqlx.Connect("postgres", databaseURL)
 		db.SetMaxOpenConns(50)
-		db.SetMaxIdleConns(100)                // The default is defaultMaxIdleConns (= 2)
+		db.SetMaxIdleConns(50)                 // The default is defaultMaxIdleConns (= 2)
 		db.SetConnMaxLifetime(5 * time.Minute) // The default is 0 (connections reused forever)
 	})
 	if err != nil {
