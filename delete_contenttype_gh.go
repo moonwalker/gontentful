85b67c5f4187fa7799ea9701aff8712052c338aa
Gaz Stevens <gaz@moonwalker.tech>
2023-05-12 15:36:23 +0200

    Push contentTypes to github

---
+++ delete_contenttype_gh.go
@@ -0,0 +1,34 @@
+package gontentful
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	gh "github.com/moonwalker/moonbase/pkg/github"
+)
+
+type GHDeleteContentType struct {
+	FolderName string
+	SysID      string
+}
+
+func NewGHDeleteContentType(sys *Sys) *GHDeleteContentType {
+	folderName := sys.ContentType.Sys.ID
+	return &GHDeleteContentType{
+		FolderName: folderName,
+		SysID:      sys.ID,
+	}
+}
+
+func (s *GHDeleteContentType) Exec(repo string) error {
+	ctx := context.Background()
+	cfg := getConfig(ctx, owner, repo, branch)
+
+	path := filepath.Join(cfg.WorkDir, s.FolderName)
+	fileNames := make([]string, 0)
+	fileNames = append(fileNames, fmt.Sprintf("%s/_schema.json", path))
+
+	_, err := gh.DeleteFiles(ctx, cfg.Token, owner, repo, branch, path, "feat(content): delete files", fileNames)
+	return err
+}
---
+++ sync_schema_gh.go
@@ -0,0 +1,46 @@
+package gontentful
+
+import (
+	"context"
+	"encoding/json"
+	"path/filepath"
+
+	gh "github.com/moonwalker/moonbase/pkg/github"
+)
+
+type GHSyncSchema struct {
+	FolderName string
+	Schema     *ContentType
+}
+
+func NewGHSyncSchema(sys *Sys, schema *ContentType) *GHSyncSchema {
+	folderName := schema.Sys.ID
+	return &GHSyncSchema{
+		FolderName: folderName,
+		Schema:     schema,
+	}
+}
+
+func (s *GHSyncSchema) Exec(repo string) error {
+	ctx := context.Background()
+	cfg := getConfig(ctx, owner, repo, branch)
+
+	ct, err := TransformModel(s.Schema)
+	if err != nil {
+		return err
+	}
+	cb, err := json.Marshal(ct)
+	if err != nil {
+		return err
+	}
+	sc := string(cb)
+	path := filepath.Join(cfg.WorkDir, s.FolderName, "_schema.json")
+
+	// Upload to github
+	_, err = gh.CommitBlob(ctx, cfg.Token, owner, repo, branch, path, &sc, "feat(content type): update files")
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
